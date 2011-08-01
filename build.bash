e95de42491d264356913fc51cdb8b745258027f0
Keith Sheppard <keithshep@gmail.com>
2011-08-01 00:42:54 -0400

    tweaked test section of build

--- build.bash
+++ build.bash
@@ -14,17 +14,22 @@ fsc --nologo --sig:LLVMFSharp.fsi --target:library --out:LLVMFSharp.dll \
 
 # uncomment the following to build and run the tests
 
+#echo "a very basic test"
 #fsc --nologo -r LLVMFSharp.dll test/simpletest.fs
 #mono simpletest.exe
-
+#
+#echo "a bit more complicated: this version uses the execution engine"
 #fsc --nologo -r LLVMFSharp.dll test/simpletest2.fs
 #mono simpletest2.exe
-
+#
+#echo "compile C code against two add functions built from LLVM"
 #fsc --nologo -r LLVMFSharp.dll test/add.fs
 #mono add.exe
 #llc -filetype=obj addModule.bc
 #gcc -o printadds addModule.o test/printadds.c
-
+#./printadds
+#
+#echo "test that the API works with C#"
 #dmcs -out:CSSimpleTest2.exe -r:LLVMFSharp.dll test/CSSimpleTest2.cs
 #mono CSSimpleTest2.exe
 
